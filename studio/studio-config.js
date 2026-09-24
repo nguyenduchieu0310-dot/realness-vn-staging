1b62286c67e94fd1c2013618a505fcbe9ad45eb8
nguyenduchieu0310-dot <nguyenduchieu0310@gmail.com>
2026-09-24 11:28:40 +0700

    Publish Studio article review workflow

--- studio/studio-config.js
+++ studio/studio-config.js
@@ -1,1 +1,2 @@
-window.REALNESS_STUDIO_API = 'https://realness-studio-api-staging.khaiphong-academy.workers.dev';
+window.REALNESS_STUDIO_API = "https://realness-studio-api-staging.khaiphong-academy.workers.dev";
+window.REALNESS_STUDIO_SITE = "staging.realness.vn";
